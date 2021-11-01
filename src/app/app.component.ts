9817e2cb5954877019b83ec521d4a90274027d8e
Febri Milansyah <febrimilan00@gmail.com>
2021-11-01 11:37:51 +0700

    Tugas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Mata Kuliah', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Jadwal Kuliah', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Nilai', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Kartu Rencana Studi', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Keungan', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
